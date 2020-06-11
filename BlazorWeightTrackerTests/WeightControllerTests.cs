81b327781dc83e29e500ce2eed49c209eb28b757
Tom Wire <tom.w.wire@gmail.com>
2020-06-11 16:06:08 +0100

    Start adding some tests for TDD

---
+++ BlazorWeightTrackerTests/WeightControllerTests.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using BlazorWeightTracker.Server.Controllers;
+using NUnit.Framework;
+
+namespace BlazorWeightTrackerTests
+{
+    public class WeightControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetWeights()
+        {
+            var controller = new WeightController();
+            var weights = controller.Get();
+
+            Assert.IsTrue(weights.Count > 0);
+        }
+    }
+}
